2749ee941fdae8c35754d5baaff769ea203f3016
ulja4a <ulja4a@gmail.com>
2023-10-24 00:16:20 +0400

    02 7 passed

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -210,7 +210,7 @@ function isPrime(/* n */) {
   /* if (n <= 1) {
     return false;
   }
-  for (let i = 2; i < n; i + 1) {
+  for (let i = 2; i <= Math.sqrt(n); i + 1) {
     if (n % i === 0) {
       return false;
     }
